f7350831f07cf6b2d2660a3ade5de9336d553d40
Srinivasan Nagarasan <srinivasan.nagarasan@baker-taylor.com>
2022-08-28 21:27:04 +0530

    content move constant

---
+++ src/language/Language.index.ts
@@ -0,0 +1,5 @@
+export const CONTENT_TEXT = {
+    MY_KERNEL: "என் கர்னல்",
+    TAG_LINE: "க ற் ற ல் - த ள ம்",
+    COMPANY_NAME: "KERNEL ROBOTICS"
+}
--- src/login/components/SplashView.tsx
+++ src/login/components/SplashView.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text} from 'react-native';
+import { CONTENT_TEXT } from '../../language/Language.index';
 
-export const SplashView = () => {
+export const SplashView = (): JSX.Element => {
     return (
     <View style={{flex:1, backgroundColor:'#ffe833'}}>
         <View style={{flex:0.99, justifyContent:'center', alignItems:'center'}}>
-            <Text style={{fontSize:53, letterSpacing:1}}>{'என் கர்னல்'}</Text>
-            <Text style={{letterSpacing:4.2}}>{'க ற் ற ல் - த ள ம்'}</Text>
+            <Text style={{fontSize:53, letterSpacing:1, fontWeight:'bold'}}>{CONTENT_TEXT.MY_KERNEL}</Text>
+            <Text style={{letterSpacing:4.2}}>{CONTENT_TEXT.TAG_LINE}</Text>
         </View>
-        <Text style={{textAlign:'center', marginBottom:30, letterSpacing:5}}>KERNEL ROBOTICS</Text>
+        <Text style={{textAlign:'center', marginBottom:30, letterSpacing:5}}>{CONTENT_TEXT.COMPANY_NAME}</Text>
     </View>);
 }
