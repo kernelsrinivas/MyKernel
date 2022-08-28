5a0cea7e76edbcf78ec46b5033408273425a0d27
Srinivasan Nagarasan <srinivasan.nagarasan@baker-taylor.com>
2022-08-28 21:14:11 +0530

    update tamil language

--- src/login/components/SplashView.tsx
+++ src/login/components/SplashView.tsx
@@ -5,9 +5,9 @@ export const SplashView = () => {
     return (
     <View style={{flex:1, backgroundColor:'#ffe833'}}>
         <View style={{flex:0.99, justifyContent:'center', alignItems:'center'}}>
-            <Text style={{fontSize:53, letterSpacing:1}}>MY KERNEL</Text>
-            <Text style={{letterSpacing:4.2}}>Robotics Learning Platform</Text>
+            <Text style={{fontSize:53, letterSpacing:1}}>{'என் கர்னல்'}</Text>
+            <Text style={{letterSpacing:4.2}}>{'க ற் ற ல் - த ள ம்'}</Text>
         </View>
-        <Text style={{textAlign:'center', marginBottom:30}}>Kernel Robotics</Text>
+        <Text style={{textAlign:'center', marginBottom:30, letterSpacing:5}}>KERNEL ROBOTICS</Text>
     </View>);
 }
